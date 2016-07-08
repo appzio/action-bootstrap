97759bd927d1634fe9f6ce4f026bbf2909ac30c6
Daniel Minchev <dogostz@gmail.com>
2016-07-08 14:07:53 +0300

    Universal component for adding suggested items

---
+++ components/ArticleSuggestedTerms.php
@@ -0,0 +1,46 @@
+<?php
+
+Yii::import('application.modules.aelogic.article.components.*');
+
+class ArticleSuggestedTerms {
+
+    public $suggestions = array();
+
+    public function getSuggestions( $key, $loosematch = false, $limit = 999 ) {
+        $words = $this->suggestions;
+
+        if ( empty($words) ) {
+            return false;
+        }
+
+        $output = array();
+        $key = strtolower($key);
+
+        foreach($words AS $word){
+            $word = strtolower($word);
+            $len = strlen($key);
+            $pointerlength = $len+1;
+            $pointer = substr($word,0,$pointerlength);
+
+            if($loosematch){
+                if(strstr($word,$key)){
+                    if(!isset($output[$pointer]) OR count($output[$pointer]) < $limit){
+                        $output[] = $word;
+                        //$output[$pointer][] = $word;
+                    }
+                }
+            } else {
+                $wordpart = substr($word,0,$len);
+                if($wordpart == $key){
+                    if(!isset($output[$pointer]) OR count($output[$pointer]) < $limit) {
+                        //$output[$pointer][] = $word;
+                        $output[] = $word;
+                    }
+                }
+            }
+        }
+
+        return $output;
+    }
+
+}
