7f1564d36748861ce9d627262be50ab5143101bf
Daniel Minchev <dogostz@gmail.com>
2017-04-07 10:58:37 +0300

    Added general style for centered text

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -755,7 +755,7 @@ class ArticleController {
     }
 
     public function getConfigParam($param,$default=false){
-    
+
         if (isset($this->configobj->$param)) {
             return $this->configobj->$param;
         } elseif ($default) {
