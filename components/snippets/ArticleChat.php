e0b6fcd8b4410fabf3b3bd087cae125395dc4a33
Appzio <info@appzio.com>
2017-04-11 18:41:53 +0300

    capitalise first name

--- components/snippets/ArticleChat.php
+++ components/snippets/ArticleChat.php
@@ -299,7 +299,7 @@ class ArticleChat extends ArticleComponent {
         }
 
         $userinfo = $this->getUserInfo($id);
-        $name = $userinfo['name'];
+        $name = ucfirst($userinfo['name']);
         $profilepic = $userinfo['profilepic'];
         $vars = $userinfo['vars'];
 
