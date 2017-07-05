495bb4555247bc3110b8e986c8830fffe6fa8250
Timo Railo <timo@appzio.com>
2017-07-05 15:57:31 +0300

    Reverting the old articlemodel

--- Models/ArticleModel.php
+++ Models/ArticleModel.php
@@ -1,38 +1,19 @@
 <?php
 
+/* here is stuff that COULD be in the Aeaction model, but its hear mainly for security purposes */
 
-namespace Article\Models;
-
-use CActiveRecord;
-use Aevariable;
-use AeplayVariable;
 
 class ArticleModel extends CActiveRecord {
 
-    use Variables;
-
     public $configobj;
-    private $varcontent;
+    public $varcontent;
 
-    public function __construct($obj){
+    /* @var ArticleController */
+    public $factory;
 
-        parent::__construct();
-        /* this exist to make the referencing of
-        passed objects & variables easier */
-
-        while($n = each($this)){
-            $key = $n['key'];
-            if(isset($obj->$key) AND !$this->$key){
-                $this->$key = $obj->$key;
-            }
-        }
-
+    public function factoryInit($data){
+        $this->factory = $data;
     }
-
-    public function tester(){
-        echo('hello');
-    }
-
 
     public function tableName(){
         return 'ae_game_branch_action';
@@ -62,6 +43,65 @@ class ArticleModel extends CActiveRecord {
         );
     }
 
+    public static function getVariables($gid){
+        $vars = Aevariable::model()->findAllByAttributes(array('game_id' => $gid));
+
+        foreach ($vars as $var) {
+            $name = $var->name;
+            $varnames[$name] = $var->id;
+        }
+
+        if(isset($varnames)){
+            return $varnames;
+        } else {
+            return false;
+        }
+    }
+
+    public static function getVariableContent($playid){
+        $vars = AeplayVariable::model()->with('variable')->findAllByAttributes(array('play_id' => $playid));
+
+        foreach($vars as $var){
+            $name = $var->variable->name;
+            $varcontent[$name] = $var->value;
+        }
+
+        if(isset($varcontent)){
+            return $varcontent;
+        } else {
+            return false;
+        }
+    }
+
+    public static function saveVariables($vars,$playid,$exclude=false){
+
+        if(is_array($vars)){
+            foreach ($vars as $var_id => $var_value) {
+                if(!isset($exclude[$var_id])){
+                    if(is_numeric($var_id)){
+                        AeplayVariable::updateWithId($playid, $var_id, $var_value);
+                    } else {
+                        /* deals mainly tags or any other format where
+                        the variable value is a list of values */
+                        if(stristr($var_id,'_')){
+                            $id = substr($var_id,0,strpos($var_id,'_'));
+                            $fieldname=substr($var_id,strpos($var_id,'_')+1);
+                            $arraysave[$id][$fieldname] = $var_value;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isset($arraysave)){
+            foreach ($arraysave as $key=>$savebit){
+                AeplayVariable::updateWithId($playid, $key, $savebit);
+            }
+        }
+
+        return true;
+
+    }
 
     public function getConfigParam($param,$default=false){
 
@@ -74,6 +114,16 @@ class ArticleModel extends CActiveRecord {
         return false;
     }
 
+    public function getSavedVariable($varname,$default=false){
+
+        if (isset($this->varcontent[$varname])) {
+            return $this->varcontent[$varname];
+        } elseif ($default) {
+            return $default;
+        }
+
+        return false;
+    }
 
 
 
